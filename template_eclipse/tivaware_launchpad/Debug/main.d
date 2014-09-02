a437dac6b5efee363cd41e6f0b4e100e3e84c1ce
Anderson Alienware - Ubuntu <anderson.igns@gmail.com>
2014-09-02 09:07:49 -0300

    Template eclipse Tiva added

---
+++ template_eclipse/tivaware_launchpad/Debug/main.d
@@ -0,0 +1,63 @@
+main.d: ../main.c /home/aignacio/tools/tivaware/inc/hw_memmap.h \
+ /home/aignacio/tools/tivaware/inc/hw_types.h \
+ /home/aignacio/tools/tivaware/driverlib/sysctl.h \
+ /home/aignacio/tools/tivaware/driverlib/gpio.h \
+ /home/aignacio/tools/tivaware/driverlib/debug.h \
+ /home/aignacio/tools/tivaware/inc/hw_timer.h \
+ /home/aignacio/tools/tivaware/driverlib/pwm.h \
+ /home/aignacio/tools/tivaware/driverlib/timer.h \
+ /home/aignacio/tools/tivaware/driverlib/pin_map.h \
+ /home/aignacio/tools/tivaware/inc/hw_gpio.h \
+ /home/aignacio/tools/tivaware/driverlib/rom.h \
+ /home/aignacio/tools/tivaware/utils/uartstdio.h \
+ /home/aignacio/tools/tivaware/utils/uartstdio.c \
+ /home/aignacio/tools/tivaware/inc/hw_ints.h \
+ /home/aignacio/tools/tivaware/inc/hw_uart.h \
+ /home/aignacio/tools/tivaware/driverlib/interrupt.h \
+ /home/aignacio/tools/tivaware/driverlib/rom_map.h \
+ /home/aignacio/tools/tivaware/driverlib/uart.h \
+ /home/aignacio/tools/tiva_arm_template/lib/servo_api.h \
+ /home/aignacio/tools/tiva_arm_template/lib/servo_api.h \
+ /home/aignacio/tools/tiva_arm_template/lib/servo_api.c
+
+/home/aignacio/tools/tivaware/inc/hw_memmap.h:
+
+/home/aignacio/tools/tivaware/inc/hw_types.h:
+
+/home/aignacio/tools/tivaware/driverlib/sysctl.h:
+
+/home/aignacio/tools/tivaware/driverlib/gpio.h:
+
+/home/aignacio/tools/tivaware/driverlib/debug.h:
+
+/home/aignacio/tools/tivaware/inc/hw_timer.h:
+
+/home/aignacio/tools/tivaware/driverlib/pwm.h:
+
+/home/aignacio/tools/tivaware/driverlib/timer.h:
+
+/home/aignacio/tools/tivaware/driverlib/pin_map.h:
+
+/home/aignacio/tools/tivaware/inc/hw_gpio.h:
+
+/home/aignacio/tools/tivaware/driverlib/rom.h:
+
+/home/aignacio/tools/tivaware/utils/uartstdio.h:
+
+/home/aignacio/tools/tivaware/utils/uartstdio.c:
+
+/home/aignacio/tools/tivaware/inc/hw_ints.h:
+
+/home/aignacio/tools/tivaware/inc/hw_uart.h:
+
+/home/aignacio/tools/tivaware/driverlib/interrupt.h:
+
+/home/aignacio/tools/tivaware/driverlib/rom_map.h:
+
+/home/aignacio/tools/tivaware/driverlib/uart.h:
+
+/home/aignacio/tools/tiva_arm_template/lib/servo_api.h:
+
+/home/aignacio/tools/tiva_arm_template/lib/servo_api.h:
+
+/home/aignacio/tools/tiva_arm_template/lib/servo_api.c:
